e3d3dcab72b1fbc7a764ec8d1ec0c37ada309bf6
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-02-03 16:22:18 +0700

    simplify crud dummy data with GO

--- main.go
+++ main.go
@@ -21,9 +21,33 @@ func getPersons(c *gin.Context) {
     c.IndentedJSON(http.StatusOK, persons)
 }
 
+func createPersons(c *gin.Context){
+    var newPerson person
+    if err := c.BindJSON(&newPerson); err != nil{
+        return
+    }
+    persons = append(persons, newPerson)
+    c.IndentedJSON(http.StatusCreated, newPerson)
+}
+func getPersonByID(c *gin.Context) {
+    id := c.Param("id")
+
+    for _, p := range persons {
+        if p.ID == id {
+            c.IndentedJSON(http.StatusOK, p)
+            return
+        }
+    }
+
+    c.JSON(http.StatusNotFound, gin.H{"error": "Person not found"})
+}
+
+
 func main() {
     router := gin.Default()
     router.GET("/persons", getPersons)
+    router.POST("/persons", createPersons)
+    router.GET("/persons/:id", getPersonByID)
 
-    router.Run("localhost:8080")
+    router.Run("localhost:8082")
 }
